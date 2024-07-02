edcb9f85eff020e7150e64c20e07959a73ec0f7e
Vishnu Simha <vishnusimha01@gmail.com>
2024-07-02 10:24:28 +0100

    Initial commit adding discovery server to chat microservices

---
+++ discoveryserver/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.2"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+//
+	implementation("javax.xml.bind:jaxb-api:2.3.0")
+	implementation("com.sun.xml.bind:jaxb-impl:2.3.0")
+	implementation("org.glassfish.jaxb:jaxb-runtime:2.3.0")
+	implementation("javax.activation:activation:1.1.1")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
